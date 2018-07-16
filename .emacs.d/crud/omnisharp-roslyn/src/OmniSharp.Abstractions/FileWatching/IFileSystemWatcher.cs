1211f7ce8f72d65296c08c4c3236357416120e4e
Reese De Wind <Reese.DeWind@gmail.com>
2018-07-16 18:06:53 -0400

    added many of the removed files back into the crud folder

---
+++ .emacs.d/crud/omnisharp-roslyn/src/OmniSharp.Abstractions/FileWatching/IFileSystemWatcher.cs
@@ -0,0 +1,16 @@
+﻿using OmniSharp.Models.FilesChanged;
+
+namespace OmniSharp.FileWatching
+{
+    public delegate void FileSystemNotificationCallback(string filePath, FileChangeType changeType);
+
+    public interface IFileSystemWatcher
+    {
+        /// <summary>
+        /// Call to watch a file or directory path for changes.
+        /// </summary>
+        /// <param name="fileOrDirectoryPath">The file or directory path to watch.</param>
+        /// <param name="callback">The callback that will be invoked when a change occurs in the watched file or directory.</param>
+        void Watch(string fileOrDirectoryPath, FileSystemNotificationCallback callback);
+    }
+}
---
+++ .emacs.d/crud/omnisharp-roslyn/src/OmniSharp.Stdio/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Services;
+using OmniSharp.Stdio.Eventing;
+
+namespace OmniSharp.Stdio
+{
+    class Program
+    {
+        static int Main(string[] args) => HostHelpers.Start(() =>
+        {
+            var application = new StdioCommandLineApplication();
+            application.OnExecute(() =>
+            {
+                // If an encoding was specified, be sure to set the Console with it before we access the input/output streams.
+                // Otherwise, the streams will be created with the default encoding.
+                if (application.Encoding != null)
+                {
+                    var encoding = Encoding.GetEncoding(application.Encoding);
+                    Console.InputEncoding = encoding;
+                    Console.OutputEncoding = encoding;
+                }
+
+                var input = Console.In;
+                var output = Console.Out;
+
+                var environment = application.CreateEnvironment();
+                OmniSharp.Configuration.ZeroBasedIndices = application.ZeroBasedIndices;
+
+                var writer = new SharedTextWriter(output);
+                var plugins = application.CreatePluginAssemblies();
+                var configuration = new ConfigurationBuilder(environment).Build();
+                var serviceProvider = CompositionHostBuilder.CreateDefaultServiceProvider(configuration);
+                var compositionHostBuilder = new CompositionHostBuilder(serviceProvider, environment, writer, new StdioEventEmitter(writer))
+                    .WithOmniSharpAssemblies();
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                var cancellation = new CancellationTokenSource();
+
+                using (var host = new Host(input, writer, environment, configuration, serviceProvider, compositionHostBuilder, loggerFactory, cancellation))
+                {
+                    host.Start();
+                    cancellation.Token.WaitHandle.WaitOne();
+                }
+
+                return 0;
+            });
+
+            return application.Execute(args);
+        });
+    }
+}
